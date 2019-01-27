f3476adc40a18ecf37dc06bfbb86d549c74cd95e
Valter Kraemer <valterkraemer@users.noreply.github.com>
2019-01-28 00:13:53 +0200

    return as string with json header

--- functions/test.js
+++ functions/test.js
@@ -1,8 +1,9 @@
 exports.handler = (event, context, callback) => {
   callback(null, {
     statusCode: 200,
-    body: {
+    body: JSON.stringify({
       value: Math.random()
-    }
+    }),
+    headers: { "Content-Type": "application/json" }
   });
 };
