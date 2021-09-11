0f0eaa6900dde9792070d33f95aa6c7c4aaa7302
Valter Kraemer <valterkraemer@users.noreply.github.com>
2021-09-11 17:39:36 +0300

    move to Cloudflare

--- functions/test.js
+++
@@ -1,9 +0,0 @@
-exports.handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      value: Math.random()
-    }),
-    headers: { "Content-Type": "application/json" }
-  });
-};
---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script>
+  const value = Math.random();
+</script>
+
+<h1>vkrae</h1>
+<h5>{value}</h5>
