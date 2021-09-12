d3419a0e5b44dac20f8a8997c74c135087f76147
Valter Kraemer <valterkraemer@users.noreply.github.com>
2021-09-12 19:20:24 +0300

    peerjs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,36 @@
-<script>
+<script lang="ts">
+  import { onMount } from "svelte";
+
   import Face from "./_Face.svelte";
+
+  let connected = false;
+
+  onMount(async () => {
+    (window as any).parcelRequire = undefined;
+    const Peer = (await import("peerjs")).default;
+
+    const peer = new Peer();
+
+    const svg = document.querySelector("svg");
+
+    peer.on("open", () => {
+      const conn = peer.connect("color-picker-yo");
+
+      conn.on("open", () => {
+        connected = true;
+        conn.on("data", (data) => {
+          console.log("Received", data);
+          if (data.color) {
+            document.body.style["background-color"] = data.color;
+          }
+        });
+      });
+
+      conn.on("close", () => {
+        connected = false;
+      });
+    });
+  });
 </script>
 
 <Face />
